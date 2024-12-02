bc5903dced7c9145763f9e07b4cb830e5c33468a
Legokichi Duckscallion <legokichi@gmail.com>
2024-12-02 15:49:36 +0900

    add autofocus app

--- autofocus/app/main.py
+++ autofocus/app/main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import argparse
 import actfw_core
+from actfw_core.autofocus import AfMode, AfSpeed, AutoFocuserIMX708
 from actfw_core.capture import V4LCameraCapture
 from actfw_core.unicam_isp_capture import UnicamIspCapture
 from actfw_core.system import find_csi_camera_device, find_usb_camera_device
 from actfw_raspberrypi.vc4 import Display
 
-# from configuration import *
-from kvs import KinesisVideoStream
 from presenter import Presenter
 from preprocess import Preprocess
 from consts import (
@@ -44,34 +43,39 @@ def main(_args):
             "vflip": False,
             "display": False,
             "capture_framerate": 8,
-            "use_usb_camera": False,
-            "stream_name": "",
-            "region_name": "",
-            "aws_access_key_id": "",
-            "aws_secret_access_key": "",
+            "use_v3_camera": False,
+            "afmode": "continuous",
+            "afvalue": 420,
+            "aftimer": 10,
         }
     )
 
     # CommandServer (for `Take Photo` command)
     cmd = actfw_core.CommandServer()
     app.register_task(cmd)
-    use_usb_camera = settings["use_usb_camera"]
 
+    # pi camera v3 のときのみ有効
+    auto_focuser = None
     try:
-        if use_usb_camera:
-            device = find_usb_camera_device()
-            cap = V4LCameraCapture(
-                device,
+        if settings["use_v3_camera"]:
+            if settings["afmode"] == "continuous":
+                mode = AfMode.AfModeContinuous
+            elif settings["afmode"] == "timer":
+                mode = AfMode.AfModeAuto
+            elif settings["afmode"] == "manual":
+                mode = AfMode.AfModeManual
+            else:
+                mode = AfMode.AfModeContinuous
+            auto_focuser = AutoFocuserIMX708(afmode=mode, afspeed=AfSpeed.AfSpeedNormal)
+            device = find_csi_camera_device()
+            cap = UnicamIspCapture(
+                unicam=device,
                 size=(CAPTURE_WIDTH, CAPTURE_HEIGHT),
                 framerate=int(settings["capture_framerate"]),
-                format_selector=V4LCameraCapture.FormatSelector.PROPER
+                hflip=settings["hflip"],
+                vflip=settings["vflip"],
+                auto_focuser=auto_focuser,
             )
-            def config(video):
-                # ignore result (requires camera capability)
-                video.set_horizontal_flip(settings["hflip"])
-                if settings['vflip']:
-                    video.set_rotation(180)
-            cap.configure(config)
         else:
             device = find_csi_camera_device()
             cap = UnicamIspCapture(
@@ -98,41 +102,32 @@ def main(_args):
     pre = Preprocess(capture_size)
     app.register_task(pre)
 
-    def run(preview_window=None):
-        if settings["stream_name"] == "":
-            kvssink = None
-        else:
-            # print("Kinesis Video Stream is enabled.")
-            kvssink = KinesisVideoStream(
-                (CAPTURE_WIDTH, CAPTURE_HEIGHT),
-                settings["stream_name"],
-                settings["region_name"],
-                settings["aws_access_key_id"],
-                settings["aws_secret_access_key"],
-            )
-            kvssink.play()
-            app.register_task(kvssink)
-
-        # Presenter task
-        pres = Presenter(preview_window, cmd, kvssink)
-        app.register_task(pres)
-
-        # Make task connection
-        cap.connect(pre)  # from `cap` to `pre`
-        pre.connect(pres)  # from `pre` to `pres
-
-        # Start application
-        app.run()
-
+    preview_window = None
     if settings["display"]:
         with Display() as display:
             preview_area = (0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT)
             with display.open_window(
                 preview_area, (CAPTURE_WIDTH, CAPTURE_HEIGHT), 16
-            ) as preview_window:
-                run(preview_window)
-    else:
-        run()
+            ) as preview_window_:
+                preview_window = preview_window_
+
+    # Presenter task
+    pres = Presenter(
+        preview_window,
+        cmd,
+        auto_focuser=auto_focuser,
+        afmode=settings["afmode"],
+        aftimer=settings["aftimer"],
+        afvalue=settings["afvalue"],
+    )
+    app.register_task(pres)
+
+    # Make task connection
+    cap.connect(pre)  # from `cap` to `pre`
+    pre.connect(pres)  # from `pre` to `pres
+
+    # Start application
+    app.run()
 
 
 if __name__ == "__main__":
--- autofocus/app/presenter.py
+++ autofocus/app/presenter.py
@@ -1,6 +1,7 @@
 from PIL import ImageDraw, ImageFont
 import numpy as np
 from actfw_core.task import Consumer
+from actfw_core.autofocus import AfMode
 import time
 import actfw_core
 from consts import INFO_COLOR, JSTDT
@@ -12,7 +13,6 @@ def to_datetime(t):
 
 
 class FPS(object):
-
     """FPS Counter"""
 
     def __init__(self, moving_average=30):
@@ -59,7 +59,15 @@ class FPS(object):
 
 
 class Presenter(Consumer):
-    def __init__(self, preview_window, cmd, kvssink):
+    def __init__(
+        self,
+        preview_window,
+        cmd,
+        auto_focuser=None,
+        afmode=None,
+        aftimer=None,
+        afvalue=None,
+    ):
         super(Presenter, self).__init__()
         self.preview_window = preview_window
         self.cmd = cmd
@@ -75,7 +83,24 @@ class Presenter(Consumer):
         self.smallfont_height = self.smallfont.getsize("99.9%")[1]
         self.fps = FPS()
 
-        self.kvssink = kvssink
+        self.auto_focuser = auto_focuser
+        self.afmode = afmode
+        self.afvalue = 420 if afvalue is None else afvalue
+        self.aftimer = 10 if aftimer is None else aftimer
+        self.aftimer_countdown = self.aftimer
+        self.prev_time = None
+
+    def focus_control(self):
+        if self.auto_focuser is not None:
+            if self.afmode == "timer":
+                if self.prev_time is not None:
+                    elapsed = time.time() - self.prev_time
+                    self.aftimer_countdown -= elapsed
+                    if self.aftimer_countdown < 0:
+                        self.auto_focuser.trigger_scan()
+                        self.aftimer_countdown = self.aftimer
+            elif self.afmode == "manual":
+                self.auto_focuser.set_focus_value(self.afvalue)
 
     def proc(self, captured_image):
         current_time = time.time()
@@ -104,11 +129,46 @@ class Presenter(Consumer):
         )
         y += self.smallfont_height
 
-        if self.kvssink is not None:
-            self.kvssink.add_image(result_image)
+        if self.auto_focuser is not None:
+            self.focus_control()
+            stats = self.auto_focuser.get_focus_stats()
+            txt = "afmode: " + str(self.afmode)
+            draw.text(
+                (2, y),
+                txt,
+                font=self.smallfont,
+                fill=INFO_COLOR,
+            )
+            y += self.smallfont_height
+            txt = "focus_val: " + str(stats.lensSetting)
+            draw.text(
+                (2, y),
+                txt,
+                font=self.smallfont,
+                fill=INFO_COLOR,
+            )
+            y += self.smallfont_height
+            txt = "aftimer: " + ":{:.2f}".format(self.aftimer_countdown)
+            draw.text(
+                (2, y),
+                txt,
+                font=self.smallfont,
+                fill=INFO_COLOR,
+            )
+            y += self.smallfont_height
+            txt = "af status: " + stats.state.name
+            draw.text(
+                (2, y),
+                txt,
+                font=self.smallfont,
+                fill=INFO_COLOR,
+            )
+            y += self.smallfont_height
 
         self.cmd.update_image(result_image)
 
         if self.preview_window is not None:
             self.preview_window.blit(np.asarray(result_image).tobytes())
             self.preview_window.update()
+
+        self.prev_time = time.time()
